c4383ef7d9d591b3ac765e6619a4f32e662fc25e
arpit-cell <57713366+arpit-cell@users.noreply.github.com>
2021-05-31 19:14:06 +0100

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,16 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+ 
+sonarqube-scanner({
+    serverUrl : 'http://34.239.255.241:9000/',
+    options: {
+      'sonar.projectName': 'NodeJS-sample',
+      'sonar.projectDescription': 'My first CI/CD app',
+      'sonar.login': "6c48a89abd5ca4aae1cb77c5b7773882c6ec4a7c",
+      'sonar.projectVersion':'1.0'
+      'sonar.language':'js',
+      'sonar.sources': 'dist',
+    //'sonar.tests': 'specs'
+    }
+  },
+  () => process.exit()
+)
