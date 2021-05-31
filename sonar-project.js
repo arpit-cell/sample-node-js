07d7b30aea52ee61db7e624500fe3d0ca91a8b6e
arpit-cell <57713366+arpit-cell@users.noreply.github.com>
2021-05-31 19:54:20 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqube-scanner({
       'sonar.projectName': 'nodejs-sample',
       'sonar.projectDescription': 'My first CI/CD app',
       'sonar.login': "6c48a89abd5ca4aae1cb77c5b7773882c6ec4a7c",
-      'sonar.projectVersion':'1.0'
+      'sonar.projectVersion': '1.0'
       'sonar.language': 'js',
       'sonar.sources': '.',
     //'sonar.tests': 'specs'
