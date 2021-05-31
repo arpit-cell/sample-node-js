c97123e4bb6b3af1cd72c8bffd49aa5b3c8cafb4
arpit-cell <57713366+arpit-cell@users.noreply.github.com>
2021-05-31 19:33:48 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqube-scanner({
     serverUrl : 'http://34.239.255.241:9000/',
     options: {
-      'sonar.projectName': 'NodeJS-sample',
+      'sonar.projectName': 'nodejs-sample',
       'sonar.projectDescription': 'My first CI/CD app',
       'sonar.login': "6c48a89abd5ca4aae1cb77c5b7773882c6ec4a7c",
       'sonar.projectVersion':'1.0'
